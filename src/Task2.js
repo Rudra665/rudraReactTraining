9c1edd20063c8ddc816f5c334394a82fecc7a08b
Rudra665 <Rudrapratapsinghbhadouria@gmail.com>
2022-10-08 15:08:00 +0530

    new file:   .vscode/settings.json 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/Task2.css 	modified:   src/Task2.js 	modified:   src/index.js

--- src/Task2.js
+++ src/Task2.js
@@ -1,50 +1,42 @@
-import {Grid} from '@mui/material';
-import Button from "@mui/material/Button";
-import { TextField } from "@mui/material";
-import './Task2.css';
-import Container from '@mui/material/Container'; 
+import * as React from "react";
+import { TextField, Grid, Typography} from "@mui/material";
+
+import Box from "@mui/material/Box";
+// import "./Task2.css";
+
+
+
 export default function Task2() {
-    return (
-        <Container maxWidth="md">
-          
-          {/* <form>
-          <Grid container spacing={2}>
-          <Grid item xs={12} md={6}> 
+  return (
   
-  </Grid>
-  <Grid item xs={12} lg={6}>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <TextField type="password" class="form-control" id="exampleInputPassword1"></TextField>
-  </div>
-  </Grid>
-  <Button type="submit" class="btn btn-primary">Submit</Button>
-  </Grid>
-</form> */}
-<div className='container'>
-<Grid container spacing={1}>
-        <Grid sm={12} md={6}>
-        <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <TextField fullWidth type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></TextField>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-        </Grid>
-        <Grid sm={12} md={6}>
-        <p>xs=6 md=8</p>
-        </Grid>
-        <Grid xs={6} md={4}>
-        <p>xs=6 md=8</p>
-        </Grid>
-        <Grid xs={6} md={8}>
-        <p>xs=6 md=8</p>
-        </Grid>
-      </Grid>
-      </div>
-      </Container>
-    )
-  }
-  
-  
-  
-  
+    
+    // <Grid component="form" container bgcolor={"aliceblue"} justifyContent="center" sm="auto" sx={{p:5, m: 6, mt:12 }}>
+    // <Typography variant="h1" align="center">Login Form</Typography>
+    // <Grid item sm={12} md={12} lg={6}>
+    // <Box
+     
+    // >
+    //   <TextField fullWidth label="Outlined secondary" color="secondary" focused />
+      
+    // </Box>
+    // </Grid>
+    // </Grid>
+    <Box
+    component="form"
+    sx={{
+      '& > :not(style)': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <TextField id="filled-basic" label="Filled" variant="filled" />
+    <TextField id="standard-basic" label="Standard" variant="standard" />
+  </Box>
+    
+    
+    
+    
+     
+  );
+}
--- src/index.js
+++ src/index.js
@@ -6,21 +6,27 @@ import Task2 from './Task2'
 import Task3 from './Task3'
 import Task4 from './Task4'
 import Task5 from './Task5'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import { StyledEngineProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  
     <BrowserRouter>
+    <StyledEngineProvider>
       <Routes>
-        <Route path="/" element={<App />}>
+        {/* <Route path="/" element={<App />}>  */}
           <Route path="/Task1" element={<Task1 />} />
           <Route path="/Task2" element={<Task2 />} />
           <Route path="/Task3" element={<Task3 />} />
           <Route path="/Task4" element={<Task4 />} />
           <Route path="/Task5" element={<Task5 />} />
-        </Route>
+         {/* </Route>  */}
       </Routes>
+      </StyledEngineProvider>
     </BrowserRouter>
+    
   </React.StrictMode>
 
 );
